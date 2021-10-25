cd54b4061072b625939417cc735180ab9ffc2d94
Adriano Diaz <adrianodpdiaz@gmail.com>
2021-10-25 12:49:49 +0200

    First commit

---
+++ core/src/com/dg/supermariobros/SuperMarioBros.java
@@ -0,0 +1,32 @@
+package com.dg.supermariobros;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.dg.supermariobros.screens.PlayScreen;
+
+public class SuperMarioBros extends Game {
+	public static final int V_WIDTH = 400;
+	public static final int V_HEIGHT = 208;
+	public SpriteBatch batch;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		setScreen(new PlayScreen(this));
+	}
+
+	@Override
+	public void render () {
+		super.render();
+	}
+
+	/*
+	@Override
+	public void dispose () {
+		batch.dispose();
+	}
+	 */
+}
---
+++ core/src/com/dg/supermariobros/screens/PlayScreen.java
@@ -0,0 +1,70 @@
+package com.dg.supermariobros.screens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.dg.supermariobros.SuperMarioBros;
+import com.dg.supermariobros.scenes.Hud;
+
+public class PlayScreen implements Screen {
+    private SuperMarioBros game;
+    private OrthographicCamera gameCam;
+    private Viewport gameViewport;
+    private Hud hud;
+
+    public PlayScreen(SuperMarioBros game) {
+        this.game = game;
+        gameCam = new OrthographicCamera();
+        gameViewport = new FitViewport(SuperMarioBros.V_WIDTH, SuperMarioBros.V_HEIGHT, gameCam);
+        hud = new Hud(game.batch);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        // Clear the screen
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
+        hud.stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        // When the size of the screen changes, the viewport gets adjusted to know what the actual screen size is
+        gameViewport.update(width, height);
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
